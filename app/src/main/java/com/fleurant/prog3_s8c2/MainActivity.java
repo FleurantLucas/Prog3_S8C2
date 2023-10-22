ef5d2e79e480d628dc5999002272e44fcdde842f
Lucas Fleurant <2291814@cegepmontpetit.ca>
2023-10-22 15:28:12 -0400

    INIT

---
+++ app/src/main/java/com/fleurant/prog3_s8c2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fleurant.prog3_s8c2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
